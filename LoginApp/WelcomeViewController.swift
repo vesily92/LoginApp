8648258f74a62f31f5dec2995c88561e4d549030
Vasilii Pronin <letstalkaboutpork@gmail.com>
2021-08-25 16:03:17 +0300

    WelcomeViewController update

---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ProfileViewController.swift
+//  LoginApp
+//
+//  Created by Василий Пронин on 24.08.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet var welcomeLabel: UILabel!
+    @IBOutlet var emojiLabel: UILabel!
+    
+    @IBOutlet var logOutButton: UIButton!
+    
+    var welcomeValue: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeLabel.textColor = .white
+        welcomeLabel.text = welcomeValue
+        emojiLabel.text = "\u{1F44B}"
+        
+        logOutButton.tintColor = .white
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [
+            UIColor.systemPink.cgColor,
+            UIColor.systemTeal.cgColor
+        ]
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+}
